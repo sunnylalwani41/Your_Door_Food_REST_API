6f4309de7d680666e349f04a1146d2168f779010
Sagar Thakur <101391397+1sagarthakur1@users.noreply.github.com>
2023-02-23 00:35:50 +0530

    day2

--- YourDoorFood/src/main/java/com/masai/model/Address.java
+++ YourDoorFood/src/main/java/com/masai/model/Address.java
@@ -1,5 +1,46 @@
 package com.masai.model;
 
-public class Address {
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@NotNull
+	private int addressid;
+	
+	@NotNull
+	@Size(max = 20)
+	private String buildingName;
+	
+	@Size(max = 10)
+	private String steetNo;
+	
+	@Size(max = 15, message = "city name is to long")
+	private String area;
+	
+	@NotNull
+	@Size(max = 5, message = "city name is to long")
+	private String city;
+	
+	@NotNull
+	private String state;
+	
+	private String country;
+	
+	@Size(min = 6,max = 6,message = "Pincode Should be 6 Digit's")
+	private int pincode;
+	
 }
