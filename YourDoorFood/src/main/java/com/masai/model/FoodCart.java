695de9bc900a0941e67ee5a98cae5d471238b4b6
sunnylalwani41 <sunnylalwani41@gmail.com>
2023-02-23 07:42:02 +0530

    update

--- YourDoorFood/src/main/java/com/masai/model/FoodCart.java
+++ YourDoorFood/src/main/java/com/masai/model/FoodCart.java
@@ -1,5 +1,33 @@
 package com.masai.model;
 
-public class FoodCart {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class FoodCart {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int cartId;
+	
+	@OneToOne
+	private Customer customer;
+	
+	@OneToMany
+	private List<Item> itemList;
+	
+	
 }
