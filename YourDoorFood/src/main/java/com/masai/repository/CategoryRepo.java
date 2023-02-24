f20e79116a740d59eda9c3f8c08cde0f73efe7b4
Dipali Sarjare <rushalisarjare1@gmail.com>
2023-02-24 10:52:46 +0530

    day4work

--- YourDoorFood/src/main/java/com/masai/repository/CategoryRepo.java
+++ YourDoorFood/src/main/java/com/masai/repository/CategoryRepo.java
@@ -1,14 +1,17 @@
 package com.masai.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.masai.model.Category;
+import com.masai.model.Item;
 import com.masai.model.Restaurant;
 
 @Repository
 public interface CategoryRepo extends JpaRepository<Category, Integer>{
 
-	
+   public Category findItemList(List<Item> list);
 
 }
--- YourDoorFood/src/main/java/com/masai/service/ICategoryService.java
+++ YourDoorFood/src/main/java/com/masai/service/ICategoryService.java
@@ -2,13 +2,14 @@ package com.masai.service;
 
 import java.util.List;
 
+import com.masai.exception.CategoryException;
 import com.masai.model.Category;
 
 public interface ICategoryService {
 
 	public Category addCategory(Category cat);
-	public Category updateCategory(Category cat);
-	public Category removeCategory(Category cat);
-	public Category viewCategory(Category cat);
-	public List<Category> viewAllCategory(Category cat);
+	public Category updateCategory(Integer catId)throws CategoryException;
+	public Category removeCategory(Integer catId)throws CategoryException;
+	public Category viewCategory(String categoryName)throws CategoryException;
+	public List<Category> viewAllCategory(Category cat)throws CategoryException;
 }
