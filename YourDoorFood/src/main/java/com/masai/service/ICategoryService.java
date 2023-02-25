ae44eaa73897b2569188c28a3ee6d8d1eeadbabb
Jagmit <bitscotch@gmail.com>
2023-02-25 22:49:57 +0530

    updated category section

--- YourDoorFood/src/main/java/com/masai/service/ICategoryService.java
+++ YourDoorFood/src/main/java/com/masai/service/ICategoryService.java
@@ -3,13 +3,10 @@ package com.masai.service;
 import java.util.List;
 
 import com.masai.exception.CategoryException;
-import com.masai.model.Category;
+import com.masai.model.Item;
 
 public interface ICategoryService {
-
-	public Category addCategory(Category cat);
-	public Category updateCategory(Integer catId)throws CategoryException;
-	public Category removeCategory(Integer catId)throws CategoryException;
-	public Category viewCategory(String categoryName)throws CategoryException;
-	public List<Category> viewAllCategory()throws CategoryException;
+	
+	public List<Item> getItemsByCategoryName(String categoryName) throws CategoryException;
+	
 }
