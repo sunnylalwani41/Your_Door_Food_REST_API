b2f1e6d22c6b5a8e972db3dda4bed66072ed2a2c
Jagmit <bitscotch@gmail.com>
2023-03-04 20:38:58 +0530

    Updated

--- YourDoorFood/src/main/java/com/masai/model/Customer.java
+++ YourDoorFood/src/main/java/com/masai/model/Customer.java
@@ -18,7 +18,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
